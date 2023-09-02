44ad41f9768b024360b0a4b16c0ec6d545a00416
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-09-02 11:35:55 -0600

    Debugging.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-import utils
+from ufs_pyutils import utils
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.join(os.path.abspath("."), "../../sorc"))

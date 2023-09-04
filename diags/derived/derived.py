2652c9211bd1f46066ce04274255055f863f788a
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-09-04 15:38:43 -0600

    Updates.

--- diags/derived/derived.py
+++ diags/derived/derived.py
@@ -1,7 +1,7 @@
 # =========================================================================
-# File: sorc/derived/derived.py
+# File: diags/derived/derived.py
 # Author: Henry R. Winterbottom
-# Date: 29 August 2023
+# Date: 09 March 2023
 # Version: 0.0.1
 # License: LGPL v2.1
 # =========================================================================

8c0ea26404be10c2c781780041341a851a62da66
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-09-28 15:44:00 -0600

    Added constants SimpleNamespace.

--- sorc/diags/derived/derived.py
+++ sorc/diags/derived/derived.py
@@ -20,6 +20,8 @@ Classes
 Requirements
 ------------
 
+- metpy; https://unidata.github.io/MetPy/latest/index.html
+
 - ufs_pyutils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
 
 Author(s)
@@ -40,11 +42,16 @@ History
 
 # ----
 
+import os
 from importlib import import_module
+from types import SimpleNamespace
 from typing import Callable, Generic
 
-from exceptions import DerivedError
+from confs.yaml_interface import YAML
+from diags.exceptions import DerivedError
+from metpy.units import units
 from tools import parser_interface
+from utils.decorator_interface import privatemethod
 from utils.logger_interface import Logger
 
 # ----
@@ -70,6 +77,69 @@ class Derived:
 
         # Define the base-class attributes.
         self.logger = Logger(caller_name=f"{__name__}.{self.__class__.__name__}")
+        self.constants_obj = self.get_constants()
+
+    @privatemethod
+    def get_constants(self: Generic) -> SimpleNamespace:
+        """
+        Description
+        -----------
+
+        This method parses an external YAML-formatted file containing
+        defined constant values and builds a SimpleNamespace object
+        containing the attributes for each of the specified constant
+        values.
+
+        Returns
+        -------
+
+        constants_obj: SimpleNamespace
+
+            A Python SimpleNamespace object containing the specified
+            constant values attributes.
+
+        Raises
+        ------
+
+        DerivedError:
+
+            - raised if an exception is raised why defining the
+              SimpleNamespace for the specified constant values
+              attributes.
+
+        """
+
+        # Collect the specified constant values and compose the
+        # base-class SimpleNamespace object.
+        constants_obj = parser_interface.object_define()
+        try:
+            constants_yaml = os.path.join(
+                parser_interface.enviro_get(envvar="DIAGS_ROOT"),
+                "parm",
+                "constants.yaml",
+            )
+            constants_dict = YAML().read_yaml(yaml_file=constants_yaml)
+        except Exception as errmsg:
+            msg = (
+                "Defining the specified constant values failed with error ",
+                f"{errmsg}. Aborting!!!",
+            )
+            raise DerivedError(msg=msg) from errmsg
+        for constant in constants_dict:
+            constant_value = constants_dict[constant]["value"]
+            constant_units = constants_dict[constant]["units"]
+            constants_obj = parser_interface.object_setattr(
+                object_in=constants_obj,
+                key=constant,
+                value=units.Quantity(
+                    constant_value,
+                    parser_interface.object_getattr(
+                        object_in=units, key=constant_units
+                    ),
+                ),
+            )
+
+        return constants_obj
 
     def get_module(self: Generic, module: str, method: str) -> Callable:
         """

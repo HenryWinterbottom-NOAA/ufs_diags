debc86a9d11b6ab4d652df6b8e7e455a41645d1d
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-12-11 16:16:44 -0700

    Updates for directory tree reconfiguration.

---
+++ sorc/grids/bearing_geoloc.py
@@ -0,0 +1,127 @@
+"""
+Module
+------
+
+    bearing_geoloc.py
+
+Description
+-----------
+
+    This module contains functions to determine locations based on
+    heading and distance.
+
+Functions
+---------
+
+    bearing_geoloc(loc1, dist, heading)
+
+        This function returns the geographical coordinate location
+        compute from a reference geographical location and the
+        distance and bearing for the destination location.
+
+Requirements
+------------
+
+- astropy; https://github.com/astropy/astropy
+
+- ufs_pyutils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
+
+Author(s)
+---------
+
+    Henry R. Winterbottom; 10 June 2023
+
+History
+-------
+
+    2023-06-10: Henry Winterbottom -- Initial implementation.
+
+"""
+
+# ----
+
+# pylint: disable=no-name-in-module
+
+# ----
+
+from math import asin, atan2, cos, sin
+from typing import Tuple
+
+import numpy
+from astropy.constants import R_earth
+from utils.logger_interface import Logger
+
+# ----
+
+# Define all available module properties.
+__all__ = ["bearing_geoloc"]
+
+# ----
+
+logger = Logger(caller_name=__name__)
+
+# ----
+
+
+def bearing_geoloc(
+    loc1: Tuple, dist: float, heading: float, radius: float = R_earth.value
+) -> Tuple[float, float]:
+    """
+    Description
+    -----------
+
+    This function returns the geographical coordinate location compute
+    from a reference geographical location and the distance and
+    bearing for the destination location.
+
+    Parameters
+    ----------
+
+    loc1: ``Tuple``
+
+        A Python tuple containing the geographical coordinates of
+        location 1; format is (lat, lon); units are degrees.
+
+    dist: ``float``
+
+        A Python float value specifying the distance from the
+        reference geographical location to the destination location;
+        units are meters.
+
+    heading: ``float``
+
+        A Python float value specifying the heading from the reference
+        geographical location to the destination location; units are
+        degrees.
+
+    Returns
+    -------
+
+    loc2: ``Tuple``
+
+        A Python tuple containing the geographical coordinates of
+        destination location; format is (lat, lon); units are degrees.
+
+    """
+
+    # Compute the new latitude and longitude geographical location.
+    (lat1, lon1, heading) = [
+        numpy.radians(loc1[0]),
+        numpy.radians(loc1[1]),
+        numpy.radians(heading),
+    ]
+    lat2 = numpy.degrees(
+        asin(
+            sin(lat1) * cos(dist / radius)
+            + cos(lat1) * sin(dist / radius) * cos(heading)
+        )
+    )
+    lon2 = numpy.degrees(
+        lon1
+        + atan2(
+            sin(heading) * sin(dist / radius) * cos(lat1),
+            cos(dist / radius) - sin(lat1) * sin(lat2),
+        )
+    )
+
+    return (lat2, lon2)
---
+++ sorc/grids/haversine.py
@@ -0,0 +1,114 @@
+"""
+Module
+------
+
+    haversine.py
+
+Description
+-----------
+
+    This module contains functions to determine geometric distances.
+
+Functions
+---------
+
+    haversine(loc1, loc2, radius=R_earth.value)
+
+        This function computes and returns the great-circle (i.e.,
+        haversine) between two locations.
+
+Requirements
+------------
+
+- astropy; https://github.com/astropy/astropy
+
+- ufs_pyutils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
+
+Author(s)
+---------
+
+    Henry R. Winterbottom; 10 June 2023
+
+History
+-------
+
+    2023-06-10: Henry Winterbottom -- Initial implementation.
+
+"""
+
+# ----
+
+# pylint: disable=no-name-in-module
+
+# ----
+
+from math import asin, cos, radians, sin, sqrt
+from typing import Tuple
+
+from astropy.constants import R_earth
+from utils.logger_interface import Logger
+
+# ----
+
+# Define all available module properties.
+__all__ = ["haversine"]
+
+# ----
+
+logger = Logger(caller_name=__name__)
+
+# ----
+
+
+def haversine(loc1: Tuple, loc2: Tuple, radius: float = R_earth.value) -> float:
+    """
+    Description
+    -----------
+
+    This function computes and returns the great-circle (i.e.,
+    haversine) between two locations.
+
+    Parameters
+    ----------
+
+    loc1: ``Tuple``
+
+        A Python tuple containing the geographical coordinates of
+        location 1; format is (lat, lon); units are degrees.
+
+    loc2: ``Tuple``
+
+        A Python tuple containing the geographical coordinates of
+        location 2; format is (lat, lon); units are degrees.
+
+    Keywords
+    --------
+
+    radius: ``float``, optional
+
+        A Python float value defining the radial distance to be used
+        when computing the haversine; units are meters.
+
+    Returns
+    -------
+
+    hvsine: ``float``
+
+        A Python float value containing the great-circle distance
+        (e.g., haversine) between the two locations defined upon
+        entry; units are meters.
+
+    """
+
+    # Define the source and destination geographical locations.
+    (lat1, lon1) = loc1
+    (lat2, lon2) = loc2
+    (lat1, lon1, lat2, lon2) = list(map(radians, [lat1, lon1, lat2, lon2]))
+
+    # Compute the great-circle distance (e.g., haversine).
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    dist = sin(dlat / 2.0) ** 2.0 + cos(lat1) * cos(lat2) * sin(dlon / 2.0) ** 2.0
+    hvsine = 2.0 * radius * asin(sqrt(dist))
+
+    return hvsine

e0854cad5abca7ef75cb5529edca66f8c1d77a35
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-12-12 15:03:10 -0700

    More additions.

--- install.sh
+++ install.sh
@@ -25,3 +25,5 @@ export INSTALL_PATH=${INSTALL_PATH:-"${PWD}"}
 echo "Installing in path ${INSTALL_PATH}"
 $(command -v pip) install --upgrade pip
 $(command -v pip) install -r "${INSTALL_PATH}/dependencies/ufs_pyutils/requirements.txt"
+$(command -v python) ${INSTALL_PATH}/dependencies/ufs_pyutils/setup.py build
+$(command -v python) ${INSTALL_PATH}/dependencies/ufs_pyutils/setup.py install

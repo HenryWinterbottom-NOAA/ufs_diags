54e3c3c8361835ff7f5e0896363062d712035522
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-12-16 12:43:16 -0700

    Updated virtual environment installation script.

--- venv/install.sh
+++ venv/install.sh
@@ -20,14 +20,26 @@
 # Configure the run-time environment.
 set -x -e
 
+# Define the environment.
 export INSTALL_PATH=${INSTALL_PATH:-"${PWD}/../"}
 export VENV_PATH="${INSTALL_PATH}/venv"
+export FC=${FC:-$(command -v which) gfortan}
 
+# Install the respective packages.
 echo "Installing in path ${INSTALL_PATH}"
 $(command -v python3) -m venv .
-$(command -v mkdir) -p "${VENV_PATH}/dependencies"
+source "${VENV_PATH}/bin/activate"
 $(command -v pip) install --upgrade pip
+$(command -v mkdir) -p "${VENV_PATH}/dependencies"
 $(command -v git) clone --recursive http://www.github.com/henrywinterbottom-noaa/ufs_pyutils --branch develop "${VENV_PATH}/dependencies/ufs_pyutils"
 $(command -v pip) install -r "${VENV_PATH}/dependencies/ufs_pyutils/requirements.txt"
 $(command -v pip) install -r "${INSTALL_PATH}/requirements.txt"
-echo "export PYTHONPATH=${VENV_PATH}/dependencies/ufs_pyutils:${INSTALL_PATH}/sorc" >> "${VENV_PATH}/bin/activate"
+
+# Build the wrapper script for the virtual environment.
+echo "Building script ${VENV_PATH}/setup.sh"
+cat >> ${VENV_PATH}/setup.sh <<EOF
+#!/usr/bin/env bash
+export PYTHONPATH="${VENV_PATH}/dependencies/ufs_pyutils:${VENV_PATH}/dependencies/ufs_diags"
+EOF
+$(command -v chmod) +x "${VENV_PATH}/setup.sh"
+
